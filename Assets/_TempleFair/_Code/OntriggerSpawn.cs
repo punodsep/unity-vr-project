61386a61cad1c2fe1e2b558ec0d0ac089f457be9
punodsep <117001403+punodsep@users.noreply.github.com>
2025-11-14 18:25:52 +0700

    Lighting incense

---
+++ Assets/_TempleFair/_Code/OntriggerSpawn.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class OntriggerSpawn : MonoBehaviour
+{
+    [Header("Trigger Settings")]
+    public GameObject targetObject;
+
+    [Header("Spawn Settings")]
+    public GameObject objectToSpawn;   // Prefab
+    public Transform spawnPoint;       // จุดเกิด
+    public Transform parentTransform;  // Parent
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == targetObject)
+        {
+            Spawn();
+        }
+    }
+
+    void Spawn()
+    {
+        if (objectToSpawn != null && spawnPoint != null)
+        {
+            Instantiate(objectToSpawn, spawnPoint.position, objectToSpawn.transform.rotation, parentTransform);
+        }
+    }
+}
