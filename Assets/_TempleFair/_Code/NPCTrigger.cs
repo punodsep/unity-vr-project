c4eb03f20301970b987d3649811efe6a29d116a8
punodsep <117001403+punodsep@users.noreply.github.com>
2025-12-01 10:25:10 +0700

    NPC

---
+++ Assets/_TempleFair/_Code/NPCTrigger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class NPCTrigger : MonoBehaviour
+{
+    public Animator anim;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // กันไม่ให้โดนอะไรแล้วติดหมด
+        {
+            anim.SetTrigger("Talk");
+            Debug.Log("Trigger NPC Animation!");
+        }
+    }
+}
