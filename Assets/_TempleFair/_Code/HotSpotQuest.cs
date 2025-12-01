fed872ab6ae1f80f03af58da65062e044eb47e6c
punodsep <117001403+punodsep@users.noreply.github.com>
2025-12-01 13:54:34 +0700

    half UI

---
+++ Assets/_TempleFair/_Code/HotSpotQuest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class HotSpotQuest : MonoBehaviour
+{
+    public GameObject imageUI;
+
+    private void Start()
+    {
+        if (imageUI != null)
+            imageUI.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            imageUI.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            imageUI.SetActive(false);
+        }
+    }
+}
